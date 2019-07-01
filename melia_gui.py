2ee6c840d2a2d6e49f5e88ad1113c9f07121b86c
Thibault <50198964+ThibGiauffret@users.noreply.github.com>
2019-07-01 12:19:09 +0200

    Add files via upload

---
+++ melia_gui.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 11:39:16 2019
+
+Melia GUI v.0.1_gui
+Algorithme de détection de mouvement basé sur une prise en rafale de photos
+
+@author: Th. G
+"""
+
+# import the necessary packages
+from skimage.measure import compare_ssim
+import imutils
+import cv2
+from shutil import copyfile
+import glob
+import os
+import datetime
+from tkinter import * 
+from PIL import Image
+from PIL import ImageTk
+import platform
+
+now = datetime.datetime.now()
+
+fenetre = Tk()
+
+fenetre.title("Melia")
+
+cadre1 = Frame(fenetre, width=600, height=50, borderwidth=1)
+cadre1.pack(side=TOP, fill=X, expand=True, anchor=N)
+
+titleLabel = Label(cadre1, font=('arial', 12, 'bold'),
+                   text="Detection de mouvement par comparaison d'images",
+                   bd=5, anchor=N)
+titleLabel.pack()
+
+cadre = Frame(fenetre, width=300, height=300, borderwidth=1)
+cadre.pack(side=LEFT, fill=X, expand=True, anchor=W)
+
+configArea = LabelFrame(cadre, text="Configuration ", width=250, height=80)
+configArea.pack(fill=X, padx=20, pady=10)
+
+
+if os.path.exists('config.txt'):
+    f= open("config.txt","r")
+    contents = f.readlines()
+    var_texte = contents[0]
+    var_texte2 = contents[1]
+else :
+    f2= open("config.txt","r+")
+    contents = []
+    contents.append(ligne_texte.get() + "\n")
+    contents.append(ligne_texte2.get() + "\n")
+    with open("config.txt", "w") as f2:
+        f2.writelines(contents)
+    f2.close()
+    
+
+f.close()
+
+label_seuil = Label(configArea, text="Seuil de similarité (de 0 à 1)")
+label_seuil.pack(anchor=W)
+
+seuil = StringVar()
+seuil.set('0.98')
+ligne_seuil = Entry(configArea, textvariable=seuil, background='white')
+ligne_seuil.pack()
+
+label2 = Label(configArea, text="Emplacement des photos")
+label2.pack(anchor=W)
+
+v = StringVar()
+v.set(var_texte.strip())
+ligne_texte = Entry(configArea, textvariable=v, background='white')
+ligne_texte.pack(fill=X)
+
+def view_source():
+    path3 = str(ligne_texte.get())
+    system = platform.system()
+    print(system)
+    if system == "Linux" :
+        os.system('xdg-open "%s"' % path3)
+    else :
+        os.startfile(path3)
+
+bouton_source = Button(configArea, text="Voir toutes les photos", command=view_source)
+bouton_source.pack(anchor=E)
+
+label3 = Label(configArea, text="Emplacement de sauvegarde")
+label3.pack(anchor=W)
+
+v2 = StringVar()
+v2.set(var_texte2.strip())
+ligne_texte2 = Entry(configArea, textvariable=v2, background='white')
+ligne_texte2.pack(fill=X)
+
+def view():
+    path3 = str(ligne_texte2.get())
+    system = platform.system()
+    print(system)
+    if system == "Linux" :
+        os.system('xdg-open "%s"' % path3)
+    else :
+        os.startfile(path3)
+
+bouton_voir = Button(configArea, text="Voir les photos extraites", command=view)
+bouton_voir.pack(anchor=E)
+
+def update_config():
+    f2= open("config.txt","r+")
+    contents = f2.readlines()
+    contents[0] = ligne_texte.get() + "\n"
+    contents[1] = ligne_texte2.get() + "\n"
+    
+    with open("config.txt", "w") as f2:
+        f2.writelines(contents)
+    f2.close()
+    print('Configuration sauvegardée !')
+
+bouton_update2 = Button(configArea, text="Sauvergarder la configuration", command=update_config)
+bouton_update2.pack(padx=20, pady=10)
+
+
+def run():
+    window = Toplevel(fenetre)
+    window.title("Detection de mouvement en cours")
+    picture = Label(window, text="Visionneuse")
+    picture.pack() 
+    
+    bouton_lancer.config(text='Tri en cours...', state=DISABLED)
+
+    counter=0
+    detect=0
+    
+    path = str(ligne_texte.get()) + "*.JPG"
+    imagePaths = glob.glob(path)
+    imagePaths.sort()
+    
+    for i in range (0,len(imagePaths),3):
+
+        first = imagePaths[i]
+        second = imagePaths[i+1]
+       
+        imageA = imread(first)
+        imageA = resize(imageA, width=min(400, imageA.shape[1]))
+        imageA = imageA[70:270,70:330]
+        imageB = imread(second)
+        imageB = resize(imageB, width=min(400, imageB.shape[1]))
+        imageB = imageB[70:270,70:330]
+    
+        grayA = cvtColor(imageA, COLOR_BGR2GRAY)
+        grayB = cvtColor(imageB, COLOR_BGR2GRAY)
+    
+        (score, diff) = compare_ssim(grayA, grayB, full=True)
+        diff = (diff * 255).astype("uint8")
+        # print("SSIM: {}".format(score))
+        
+        
+        n_seuil = float(ligne_seuil.get())
+        
+        if score <= n_seuil :
+            
+            thresh = cv2.threshold(diff, 0, 255,
+            	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+            	cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+        
+            for c in cnts:
+                	(x, y, w, h) = cv2.boundingRect(c)
+                	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            
+            im = Image.fromarray(imageA)
+            imgtk = ImageTk.PhotoImage(image=im)
+            picture.config(image=imgtk)
+            fenetre.update()
+            print(imagePaths[i] + ' sauvegardée !')
+            path2 = str(ligne_texte2.get()) 
+            try:
+                os.mkdir(path2 + now.strftime("%Y-%m-%d"))
+            except FileExistsError:
+                pass
+            copyfile(imagePaths[i], path2 + now.strftime("%Y-%m-%d") + '/img_'+ str(counter) + '.jpg')
+            detect+=1
+            
+        counter+=1
+        
+    label4 = Label(cadre, text="Nombre de mouvements détectés : {}".format(detect))
+    label4.pack()
+    bouton_lancer.config(text='Lancer la détection !', state="normal")
+    window.destroy()
+
+
+bouton_lancer = Button(cadre, text="Lancer la détection !", command=run)
+bouton_lancer.pack(padx=20, pady=20)
+
+fenetre.mainloop() 
+
+#    # threshold the difference image, followed by finding contours to
+#    # obtain the regions of the two input images that differ
+#    thresh = cv2.threshold(diff, 0, 255,
+#    	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+#    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+#    	cv2.CHAIN_APPROX_SIMPLE)
+#    cnts = imutils.grab_contours(cnts)
+#
+#    # loop over the contours
+#    for c in cnts:
+#    	# compute the bounding box of the contour and then draw the
+#    	# bounding box on both input images to represent where the two
+#    	# images differ
+#    	(x, y, w, h) = cv2.boundingRect(c)
+#    	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+#    	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+#
+## show the output images
+#cv2.imshow("Original", imageA)
+#cv2.imshow("Modified", imageB)
+#cv2.imshow("Diff", diff)
+#cv2.imshow("Thresh", thresh)
+#cv2.waitKey(0)
---
+++ melia_simple.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 11:39:16 2019
+
+Melia (Console version) v.0.1_console
+Algorithme de détection de mouvement basé sur une prise en rafale de photos
+
+@author: Th. G
+"""
+
+# Importation des librairies
+from skimage.measure import compare_ssim
+from imutils import paths
+from imutils import resize
+from cv2 import cvtColor
+from cv2 import COLOR_BGR2GRAY
+from cv2 import imread
+from shutil import copyfile
+import glob
+from os import path
+from os import mkdir
+import datetime
+
+print('Initialisation...')
+
+now = datetime.datetime.now()
+
+counter=0
+detect=0
+
+if path.exists('config.txt'):   # On vérifie si le fichier de configuration existe.
+    f= open("config.txt","r")
+    contents = f.readlines()
+    var = contents[0]   # La première ligne est l'emplacement des photos. 
+    var2 = contents[1]  # La seconde ligne est l'emplacement de sauvegrarde. 
+    print('Configuration chargée !')    
+else :
+    print('Erreur de chargement de la configuration :/')
+
+path = var.strip() + "*.JPG"    # On récupère les adresses de toutes les images au format JPG
+imagePaths = glob.glob(path)
+imagePaths.sort()   # On les tris par ordre alphabétique
+
+for i in range (0,len(imagePaths),3):   # Dans le cas d'un prise en rafale de 3 images, on parcours toutes les images par groupe de 3.
+    print('Traitement en cours...')
+    first = imagePaths[i]   
+    second = imagePaths[i+1]    
+   
+    imageA = imread(first)  # On charge la première image de la rafale.
+    imageA = resize(imageA, width=min(400, imageA.shape[1]))    # On réduit la résolution pour améliorer le temps de détection et la précision.
+    imageA = imageA[70:270,70:330]  # On definit une ROI qui correspond à la zone où on souhaite détecter.
+    imageB = imread(second) # On charge la seconde image de la rafale.
+    imageB = resize(imageB, width=min(400, imageB.shape[1]))
+    imageB = imageB[70:270,70:330]
+
+    grayA = cvtColor(imageA, COLOR_BGR2GRAY) # On passe en niveaux de gris.
+    grayB = cvtColor(imageB, COLOR_BGR2GRAY)
+
+    (score, diff) = compare_ssim(grayA, grayB, full=True)   # On calcule de Structural Similarity Index (SSIM).
+    diff = (diff * 255).astype("uint8")
+    
+    if score <= 0.98 :  # Le seuil fixe est choisit expérimentalement 0.98.
+        
+        print(imagePaths[i] + ' sauvegardée !')
+        path2 = var2.strip()
+        try:
+            mkdir(path2 + now.strftime("%Y-%m-%d"))     # On crée un dossier à la date du jour.
+        except FileExistsError:
+            pass
+        copyfile(imagePaths[i], path2 + now.strftime("%Y-%m-%d") + '/img_'+ str(counter) + '.jpg')  # On copie les images du mouvement.
+        detect+=1
+        
+    counter+=1
+
+print('Terminé, nombre de mouvements détectés : ' + str(detect))
